1f15ea83db47194b1b0067c8f6fa0de0e2b52487
Antoine Darveau-Bernier <adb@astro.umontreal.ca>
2021-11-17 09:53:04 -0500

    Correct bug in check_dispersioon_direction. Clean up code.

--- jwst/extract_1d/soss_extract/soss_extract.py
+++ jwst/extract_1d/soss_extract/soss_extract.py
@@ -551,7 +551,6 @@ def run_extract1d(input_model: DataModel,
         for order in tracemodels:
             # Save as a list (convert to array at the end)
             all_tracemodels[order] = [tracemodels[order]]
-            print(order, np.array(all_tracemodels[order]).shape)
 
         # Use the trace models to perform a de-contaminated extraction.
         kwargs = dict()
@@ -566,7 +565,6 @@ def run_extract1d(input_model: DataModel,
         for order in box_weights:
             # Save as a list (convert to array at the end)
             all_box_weights[order] = [box_weights[order]]
-            print('Box weights', order, np.array(all_box_weights[order]).shape)
 
         # Copy spectral data for each order into the output model.
         # TODO how to include parameters like transform and tikfac in the output.
@@ -642,8 +640,6 @@ def run_extract1d(input_model: DataModel,
                 if i == 0:
                     all_tracemodels[order] = []
                 all_tracemodels[order].append(tracemodels[order])
-                print(order, np.array(all_tracemodels[order]).shape)
-
 
             # Use the trace models to perform a de-contaminated extraction.
             kwargs = dict()
@@ -660,7 +656,6 @@ def run_extract1d(input_model: DataModel,
                 if i == 0:
                     all_box_weights[order] = []
                 all_box_weights[order].append(box_weights[order])
-                print('Box weights', order, np.array(all_box_weights[order]).shape)
 
             # Copy spectral data for each order into the output model.
             # TODO how to include parameters like transform and tikfac in the output.
